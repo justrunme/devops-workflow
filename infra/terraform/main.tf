1fa071751ab9ebe33b880c4ea606d8243207d09f
justrunme <justrunme@gmail.com>
2025-06-27 22:31:57 +0200

    Fix: Remove duplicate variable declarations in main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -167,12 +167,3 @@ resource "kubernetes_deployment" "backend_worker" {
   }
 }
 
-variable "docker_username" {
-  description = "Your Docker Hub username or GitHub Container Registry username."
-  type        = string
-}
-
-variable "image_tag" {
-  description = "The Docker image tag to deploy."
-  type        = string
-}
